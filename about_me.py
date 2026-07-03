0d928ca34421342460a5f1f04b79df753cd6150d
Norsky52 <freedomhelper@gmail.com>
2026-07-02 20:40:38 -0700

    Add personal introduction program using variables and f-strings

---
+++ about_me.py
@@ -0,0 +1,26 @@
+# introduction.py
+# A personal introduction program using variables and f-strings
+# Personal details
+first_name = "Jan"
+last_name = "Ostebo"
+age = 56
+city ="Seaside"
+current_job = "Merchandiser"
+#display the formatted introduction
+print('=' * 40)
+print("Personal Introduction")
+print('=' * 40)
+print(f"Name:{first_name} {last_name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print(f"Current career: {current_job}")# This tell what i am doing.
+print(f"Why am i learning to code?") 
+print(f"I want to learn to make a difference and make good money!.")
+print(f"fun fact: I can draw portraits and landscapes.")
+print('=' * 40)
+
+print(f"{first_name} is {age} years old and lives in {city}.")
+print(f"I am learning Python to build AI systems.")
+print('=' * 40)#added this section to show major area of interest.
+
+
